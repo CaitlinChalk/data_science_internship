0be54ed7ab06bf4a97ead2b6e7f9160ca9c6769c
CaitlinChalk <matcc@leeds.ac.uk>
2019-10-11 12:09:57 +0100

    Separate Saudi data into relevant components

--- project1/python_scripts/read_data1.py
+++ project1/python_scripts/read_data1.py
@@ -1,15 +1,36 @@
 """
 Script to open and re-structure Saudi data
+raw data structure:
+    usual brand, laundry habits, misc opinions (inc overall rating), ratings, agreements, attitudes, Person Factors
 """
 
 import pandas as pd
+import numpy as np
 
-data = pd.read_excel('../data/raw/Saudi_data.xlsx')
+data = pd.read_excel('../data/raw/Saudi_data.xlsx') #read raw data
 
-col_id = data.columns.get_loc("Proportion Of Loads Used Liquid Bleach Together With Regular Laundry Detergent In Past 6 Months")
+#separate data 
 
-laundry_habits = data.iloc[:,0:col_id+1]
+#column dividers for data separation
+c1 = "Usual Brand P6M"
+c2 = "Whether Usually Use Liquid Bleach - Personal"
+c3 = "Rating For Cleaning Laundry Overall"
+c4 = "Agreement With This Product Provides Excellent Value"
+c5 = "Attitude Towards Benefit Removes All Greasy Food Stains With No Pretreating (Scrubbing Or Extra Products)"
+c6 = "Sex Of Respondent"
 
-col_id2 = data.columns.get_loc("Moment Of Greatest Frustration/ Disappointment About Usual Laundry Detergent")
+usual_brand = data.loc[:,c1] #list of usual landry brands (i.e. facets in Rasch analysis)
+    
+laundry_habits = data.loc[:,c1+1:c2]
 
-usual_laundry_rating = data.iloc[:,col_id+1:col_id2+1]
+laundry_opinions = data.loc[:,c2+1:c3]
+
+ratings = data.loc[:,c3+1:c4]
+
+agreements = data.loc[c4+1:c5]
+
+person_factors = data.loc[c5+1:c6]
+
+#restructure for Rasch analysis
+
+facets = np.unique(usual_brand) #count number of distinct laundry brands
