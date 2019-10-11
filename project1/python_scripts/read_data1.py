e318e5a7e34b1a3b2de8e6c22ac408ea674d335a
CaitlinChalk <matcc@leeds.ac.uk>
2019-10-11 16:50:35 +0100

    add commands to put data into RUMM form includes: separation of raw data into different components, dictionary to convert strings to numbers, replacement of facets with integer

--- project1/python_scripts/read_data1.py
+++ project1/python_scripts/read_data1.py
@@ -9,28 +9,120 @@ import numpy as np
 
 data = pd.read_excel('../data/raw/Saudi_data.xlsx') #read raw data
 
-#separate data 
+#replace NaN with -1
+#data.fillna(-1)
+data.replace(np.nan,-1,inplace=True)
+#separate raw data into different components 
+#------------------------------------------------------------------------------
 
 #column dividers for data separation
 c1 = "Usual Brand P6M"
-c2 = "Whether Usually Use Liquid Bleach - Personal"
-c3 = "Rating For Cleaning Laundry Overall"
-c4 = "Agreement With This Product Provides Excellent Value"
-c5 = "Attitude Towards Benefit Removes All Greasy Food Stains With No Pretreating (Scrubbing Or Extra Products)"
-c6 = "Sex Of Respondent"
+c2 = "Brands Of Laundry Detergents Used In Past 6 Months - Household"
+c3 = "Overall Rating For Usual Laundry Detergent"
+c4 = "Rating For Cleaning Laundry Overall"
+c5 = "Agreement With This Product Provides Excellent Value"
+c6 = "Attitude Towards Benefit Removes All Greasy Food Stains With No Pretreating (Scrubbing Or Extra Products)"
+c7 = "Sex Of Respondent"
 
-usual_brand = data.loc[:,c1] #list of usual landry brands (i.e. facets in Rasch analysis)
+#column indices for column names 
+c1i = data.columns.get_loc(c1) 
+c2i = data.columns.get_loc(c2) 
+c3i = data.columns.get_loc(c3) 
+c4i = data.columns.get_loc(c4) 
+c5i = data.columns.get_loc(c5) 
+c6i = data.columns.get_loc(c6) 
+c7i = data.columns.get_loc(c7) 
+
+usual_brand = data.iloc[:,c1i] 
+ 
+laundry_habits = data.iloc[:,c2i:c3i]
+
+laundry_opinions = data.iloc[:,c3i:c4i]
+
+ratings = data.iloc[:,c4i:c5i]
+
+agreements = data.iloc[:,c5i:c6i]
+
+attitudes = data.iloc[:,c6i:c7i]
+
+person_factors = data.iloc[:,c7i:len(data.columns)]
+
+del data
+
+#sort components further:
+#------------------------------------------------------------------------------
+
+#some columns in laundry_opinions belong in ratings instead
+#labels of rating columns in laundry_options
+r1 = "Overall Rating For Usual Laundry Detergent"
+r2 = "Relative Category Rating For Usual Laundry Detergent"
+r3 = "Performance vs. Expectations For Usual Laundry Detergent"
+r4 = "Distinctiveness Vs Other Products For Usual Laundry Detergent"
+r5 = "Value For Price/ Money For Usual Laundry Detergent"
+
+#add extra ratings from laundry_opinions to the ratings data matrix
+ratings['Overall Product Rating'] = laundry_opinions.loc[:,r1]
+ratings['Relative Category Rating'] = laundry_opinions.loc[:,r2]
+ratings['Performance vs Expectation'] = laundry_opinions.loc[:,r3]
+ratings['Distinctiveness'] = laundry_opinions.loc[:,r4]
+ratings['Value for money'] = laundry_opinions.loc[:,r5]
+
+#separate purchase intent as a variable (to analyse later if required)
+purchase_intent = laundry_opinions.loc[:,"Purchase Intent For Usual Laundry Detergent"]
+
+#remove purchase intent and ratings from laundry_opinions (to leave opininions only)
+laundry_opinions = laundry_opinions.drop(columns="Purchase Intent For Usual Laundry Detergent")
+laundry_opinions = laundry_opinions.drop(columns=[r1,r2,r3,r4,r5])
+
+#separate respondent id from person_factors
+id1 = person_factors.loc[:,"Respondent Serial"]  
+#remove id, sex (not necessary, all female) and US$ income (use SR instead) from person_factors
+person_factors = person_factors.drop(columns="Respondent Serial")
+person_factors = person_factors.drop(columns="Sex Of Respondent")
+person_factors = person_factors.drop(columns="Household Income US$")
+
+#combine attitudes and opinions
+attitudes_and_opinions = pd.concat([attitudes, laundry_opinions], axis=1, sort=False) #concatenates horizontally
+del [attitudes, laundry_opinions]
+
+#%% restructure for Rasch analysis
+#Rasch input file structure: id, facets, PFs, items
+#------------------------------------------------------------------------------
+
+facet_list = np.unique(usual_brand) #count number of distinct laundry brands
+
+#create dictionary for facets, and replace brands with integers 
+facets = usual_brand
+facet_dict = {}
+for i in range(len(facet_list)):
+    facet_dict[i] = {facet_list[i]: i}
+    facets.replace(facet_dict[i], inplace=True)
     
-laundry_habits = data.loc[:,c1+1:c2]
+#%% person factors
+   
+age_list = np.unique(person_factors.loc[:,"Age Of Respondent (ageres)"])
+    
 
-laundry_opinions = data.loc[:,c2+1:c3]
+# to do: REMOVE NANS FIRST
 
-ratings = data.loc[:,c3+1:c4]
 
-agreements = data.loc[c4+1:c5]
 
-person_factors = data.loc[c5+1:c6]
 
-#restructure for Rasch analysis
 
-facets = np.unique(usual_brand) #count number of distinct laundry brands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
