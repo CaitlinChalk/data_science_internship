9721b32952facc2cd1c1d1d74008523f76aa4a5e
CaitlinChalk <matcc@leeds.ac.uk>
2019-10-30 17:33:12 +0000

    output extreme persons as array

--- project1/python_scripts/person_analysis.py
+++ project1/python_scripts/person_analysis.py
@@ -10,26 +10,28 @@ import pandas as pd
 import numpy as np
 from data_manipulation import ammend_facet_key
 
-data = pd.read_excel('../data/Data1_saudi/usual15_persons.xlsx') #individual person fit data
+data = pd.read_excel('../data/Data1_saudi/facet7_extreme_persons.xlsx') #individual person fit data
 
 #column indicating extreme people
 extremes = data.loc[:,'Extm']
 
 #ID of extreme persons
 extreme_index = extremes[extremes=='extm'].index #index of extreme persons
-extreme_ID = data.loc[extreme_index,'Unnamed: 10'] #extreme person IDs
-extreme_ID_index = extreme_ID.index #index of extreme person IDs
-extreme_ID.sort_values(inplace=True) #put in ascending order
-n_extremes = len(extreme_ID) #number of extreme persons
+extreme_persons = extreme_index.values+1
 
-facet_new, extreme_facets = ammend_facet_key(facets_RUMM,facets_key,extreme_ID)
+#extreme_ID = data.loc[extreme_index,'Unnamed: 10'] #extreme person IDs
+#extreme_ID_index = extreme_ID.index #index of extreme person IDs
+#extreme_ID.sort_values(inplace=True) #put in ascending order
+#n_extremes = len(extreme_ID) #number of extreme persons
+
+#facet_new, extreme_facets = ammend_facet_key(facets_RUMM,facets_key,extreme_ID)
 
 #%%
 
-facet = 2
-facetID = facets_RUMM[facets_RUMM==facet] #.index
+facet = 7
+facetID = facets_RUMM[facets_RUMM==facet-1] #.index
 persons = facetID.index.values+1
-PID = pd.Series(data=facetID.index.values+1)
+#PID = pd.Series(data=facetID.index.values+1)
 #persons = data.loc[extreme_index,'Unnamed: 10'] 
 
         
